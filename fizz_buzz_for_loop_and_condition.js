b154f903abb505e4d5d87aa33fcfe73b895091c1
Sarah Caulfield <43242833+scaulfield7@users.noreply.github.com>
2021-02-11 21:33:24 +0000

    Second commit
    Added JavaScript file

---
+++ fizz_buzz_for_loop_and_condition.js
@@ -0,0 +1,25 @@
+/*
+    Writes a program that prints the numbers from 1 to 100,
+    but for multiples of 3, prints "is divisible by 3" along
+    with the number, and for multiples of 5, prints them with
+    "can be divided by 5". For numbers which are multiples of
+    both 3 and 5, it prints "is divisible by 3 and 5".
+*/
+
+let x = 1;
+
+for(var i = x; i <= 100; i++) {
+    if(x % 3 === 0 && x % 5 === 0) {
+        console.log(x + " is divisible by 3 and 5");
+    }
+    else if(x % 3 === 0) {
+        console.log(x + " is divisible by 3");
+    }
+    else if(x % 5 === 0) {
+        console.log(x + " can be divided by 5");
+    }
+    else {
+        console.log(x);
+    }
+    x++;
+}
